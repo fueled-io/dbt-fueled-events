e4a9b4ea4fd2ddf820d08c60a8004548720361d2
jason@cutsclothing.com <jason@cutsclothing.com>
2025-03-13 14:16:11 +0000

    re defined attribution models

--- models/gold/fueled_events_gold_product_removed.sql
+++ models/gold/fueled_events_gold_product_removed.sql
@@ -5,9 +5,11 @@
 select 
   s.session_id,
   pr.*,
-  s.marketing_source as session_marketing_soure,
-  s.marketing_medium as session_marketing_medium
+  s.session_first_touch_marketing_source,
+  s.session_last_touch_marketing_source,
+  s.session_first_touch_marketing_medium,
+  s.session_last_touch_marketing_medium
 from {{ ref('fueled_events_silver_product_removed') }} as pr
-left join {{ ref('fueled_events_silver_session_info') }} as s
+left join {{ ref('fueled_events_silver_session_touch_level_attributes') }} as s
   on pr.anonymous_id = s.context_anonymous_id 
 qualify row_number() over(partition by pr.event_id order by abs(UNIX_SECONDS(s.session_start)-UNIX_SECONDS(pr.original_timestamp))) = 1
