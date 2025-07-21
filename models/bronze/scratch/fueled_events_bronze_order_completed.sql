50dd23410324bf84dd5a6681e480a2e7cfce3fa8
shahriar-fueled <shahriar@fueled.io>
2025-07-21 12:20:26 +0600

    order completed s3 destionartion added

--- models/bronze/scratch/fueled_events_bronze_order_completed.sql
+++ models/bronze/scratch/fueled_events_bronze_order_completed.sql
@@ -6,10 +6,6 @@
 with
     source as (
         select * from {{ source("fueled_events_atomic", "order_completed") }}
-        {% if var('include_s3_destination_data', false) %}
-            union all
-            select * from {{ref("s3_destination_order_completed_raw")}}
-        {% endif %}
     ),
 
     order_completed as (
@@ -18,7 +14,7 @@ with
             affiliation,
             billing_address_address,
             billing_address_city,
-            billing_address_company
+            billing_address_company,
             billing_address_country,
             billing_address_first_name,
             billing_address_last_name,
@@ -48,6 +44,43 @@ with
             id as event_id
 
         from source
+        {% if var('include_s3_destination_data', false) %}
+            where original_timestamp< '{{var('s3_destination_date', '2025-07-21')}}'
+            union all
+            select affiliation,
+            billing_address_address,
+            billing_address_city,
+            billing_address_company,
+            billing_address_country,
+            billing_address_first_name,
+            billing_address_last_name,
+            billing_address_phone,
+            billing_address_postal_code,
+            billing_address_state,
+            cart_id,
+            coupon,
+            currency,
+            discount,
+            event_text,
+            order_id,
+            payment_type,
+            products,
+            revenue,
+            shipping,
+            status,
+            subtotal,
+            tax,
+            total,
+            type,
+            value,
+            coalesce(context_fueled_external_id, context_anonymous_id) as anonymous_id,
+            user_id,
+            original_timestamp
+            {{ get_shared_event_attributes("fueled_events_atomic", "order_completed") }},
+            event_id
+            from {{ref("s3_destination_order_completed_raw")}}
+            where original_timestamp>= '{{var('s3_destination_date', '2025-07-21')}}'
+        {% endif %}
 
     )
 
